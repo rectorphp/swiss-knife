0d1c99c9da99800b10ec6d7f6173f12a7d7dab5c
GitHub Action <action@github.com>
2022-06-08 17:43:10 +0000

    Updated Easy CI to commit eca86fd1d8a72041ec1d339825bff1b897b9eb0c
    https://github.com/symplify/symplify/commit/eca86fd1d8a72041ec1d339825bff1b897b9eb0c [PHPStanRules] Remove parent from NoMissingDirPathRule (#4125)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit370b3833eafc37affcd55a6307e62536::getLoader();
+return ComposerAutoloaderInit258744a1f93769f584db3e8764f83bde::getLoader();
