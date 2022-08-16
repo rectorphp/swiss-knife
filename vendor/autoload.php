4d3f94c99c583b19c41aed9ea3f1cf929619b3c9
GitHub Action <action@github.com>
2022-08-16 22:30:55 +0000

    Updated Easy CI to commit b755d8b7ce82f1022a8d9a23ce988359f94b10ea
    https://github.com/symplify/symplify/commit/b755d8b7ce82f1022a8d9a23ce988359f94b10ea [PHPStanRules] Add ReturnTypeDeclarationSeaLevelRule (#4347)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb869db61b67dd59581705346370b0da5::getLoader();
+return ComposerAutoloaderInitf29e25987cc8257e3007e3f89d453b17::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitb869db61b67dd59581705346370b0da5
+class ComposerStaticInitf29e25987cc8257e3007e3f89d453b17
 {
     public static $files = array (
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
@@ -1168,9 +1168,9 @@ class ComposerStaticInitb869db61b67dd59581705346370b0da5
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitb869db61b67dd59581705346370b0da5::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitb869db61b67dd59581705346370b0da5::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInitb869db61b67dd59581705346370b0da5::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitf29e25987cc8257e3007e3f89d453b17::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitf29e25987cc8257e3007e3f89d453b17::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitf29e25987cc8257e3007e3f89d453b17::$classMap;
 
         }, null, ClassLoader::class);
     }
