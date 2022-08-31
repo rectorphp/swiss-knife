3710227858ba00601d332f97b2a45905c20ba951
GitHub Action <action@github.com>
2022-08-31 21:56:13 +0000

    Updated Easy CI to commit 161760240721f37172a451efc33fba7ec5cd02a5
    https://github.com/symplify/symplify/commit/161760240721f37172a451efc33fba7ec5cd02a5 Proper support for invokable factories/configurators (#4379)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4d7161f4720cf88ca42d0a4122521d38::getLoader();
+return ComposerAutoloaderInit1056f0e875e55bf526ab7048a4d7b323::getLoader();
