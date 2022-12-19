5eb5a3cc7995389942d729281a6551c09348d1de
GitHub Action <action@github.com>
2022-12-19 12:13:44 +0000

    Easy CI 11.1.19

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5afba9a2b29471a7894a3f328dbbf487::getLoader();
+return ComposerAutoloaderInit21226b9462fd3f03abec79dd26757ba4::getLoader();
