e827886db7366e7f441f4f62950b3b09900227ef
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-06 18:14:04 +0700

    add types to skip

--- easy-ci.php
+++ easy-ci.php
@@ -2,13 +2,14 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\Console\Application;
 use Symplify\EasyCI\Config\EasyCIConfig;
-use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
 
 return static function (EasyCIConfig $easyCIConfig): void {
     $easyCIConfig->typesToSkip([
-        AbstractSymplifyKernel::class,
-        Application::class,
+        \Symplify\EasyCI\Config\EasyCIConfig::class,
+        \Symplify\EasyCI\Contract\Application\FileProcessorInterface::class,
+        \Symplify\EasyCI\Twig\TwigTemplateAnalyzer\ConstantPathTwigTemplateAnalyzer::class,
+        \Symplify\EasyCI\Twig\TwigTemplateAnalyzer\MissingClassConstantTwigAnalyzer::class,
+        \Symplify\EasyCI\ValueObject\ConfigFileSuffixes::class,
     ]);
 };
