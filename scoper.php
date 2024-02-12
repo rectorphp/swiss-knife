741bf927351495b8cfc4822b43815da1589bf50b
Tomas Votruba <tomas.vot@gmail.com>
2024-02-12 21:57:43 +0100

    scope

--- scoper.php
+++ scoper.php
@@ -12,9 +12,9 @@ use Nette\Utils\Strings;
 
 // see https://github.com/humbug/php-scoper
 return [
-    'prefix' => 'EasyCI' . $timestamp,
+    'prefix' => 'SwissKnife' . $timestamp,
     'expose-constants' => ['#^SYMFONY\_[\p{L}_]+$#'],
-    'exclude-namespaces' => ['#^Symplify\\\\EasyCI#', '#^Symfony\\\\Polyfill#'],
+    'exclude-namespaces' => ['#^Rector\\\\SwissKnife#', '#^Symfony\\\\Polyfill#'],
     'exclude-files' => [
         // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
         // these paths are relative to this file location, so it should be in the root directory
