bf781d4ea9047e6a679822215ca64b0f0732ee7f
Tomas Votruba <tomas.vot@gmail.com>
2024-03-06 22:10:31 +0100

    cs

--- src/FileSystem/PathHelper.php
+++ src/FileSystem/PathHelper.php
@@ -6,16 +6,16 @@ namespace Rector\SwissKnife\FileSystem;
 
 final class PathHelper
 {
-    private static function normalize(string $filePath): string
-    {
-        return str_replace('\\', '/', $filePath);
-    }
-
     public static function relativeToCwd(string $filePath): string
     {
         $filePath = self::normalize($filePath);
 
         // get relative path from getcwd()
         return str_replace(self::normalize((string) getcwd()) . '/', '', $filePath);
+    }
+
+    private static function normalize(string $filePath): string
+    {
+        return str_replace('\\', '/', $filePath);
     }
 }
