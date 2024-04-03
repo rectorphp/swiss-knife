81c62e8490a031524ecd5cf15d7873eaa474f224
Markus Staab <m.staab@complex-it.de>
2024-04-03 17:25:31 +0200

    Support root-namespace parent classes

---
+++ tests/ParentClassResolver/Fixture/SomeClassWithInternalParent.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\SwissKnife\Tests\ParentClassResolver\Fixture;
+
+final class SomeClassWithInternalParent extends \ArrayObject
+{
+}
--- tests/ParentClassResolver/ParentClassResolverTest.php
+++ tests/ParentClassResolver/ParentClassResolverTest.php
@@ -7,9 +7,7 @@ namespace Rector\SwissKnife\Tests\ParentClassResolver;
 use Rector\SwissKnife\Finder\PhpFilesFinder;
 use Rector\SwissKnife\ParentClassResolver;
 use Rector\SwissKnife\Tests\AbstractTestCase;
-use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\AbstractParentClass;
-use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClass;
-use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClassInSeparateNamespace;
+use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\SomeParentClass;
 
 final class ParentClassResolverTest extends AbstractTestCase
 {
@@ -24,14 +22,17 @@ final class ParentClassResolverTest extends AbstractTestCase
 
     public function test(): void
     {
-        $parentClasses = $this->parentClassResolver->resolve(
-            PhpFilesFinder::find([__DIR__ . '/Fixture']),
-            static function (): void {
-            }
-        );
+        $parentClasses = $this->parentClassResolver->resolve(PhpFilesFinder::find([__DIR__ . '/Fixture']), static function (): void {
+        });
 
         $this->assertSame(
-            [AbstractParentClass::class, ParentClass::class, ParentClassInSeparateNamespace::class],
+            [
+                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\AbstractParentClass',
+                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClass',
+                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClassInSeparateNamespace',
+                SomeParentClass::class,
+                \SomeUnknownRootNamespaceClass::class, // @phpstan-ignore-line
+            ],
             $parentClasses
         );
     }
