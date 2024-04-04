b2b162ec2402031de414168412323538b0aa29f0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-04-05 01:53:02 +0700

    [Comments] Handle new line mix betweeen \n and \r\n in the code on CommentedCodeAnalyzer (#23)

--- src/Comments/CommentedCodeAnalyzer.php
+++ src/Comments/CommentedCodeAnalyzer.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\SwissKnife\Comments;
 
 use Nette\Utils\FileSystem;
+use Nette\Utils\Strings;
 
 /**
  * @see \Rector\SwissKnife\Tests\Comments\CommentedCodeAnalyzerTest
@@ -12,13 +13,20 @@ use Nette\Utils\FileSystem;
 final class CommentedCodeAnalyzer
 {
     /**
+     * @var string
+     * @see https://regex101.com/r/5OlGjG/1
+     * @see https://3v4l.org/Y8pSD
+     */
+    private const NEWLINE_REGEX = '#\r?\n#';
+
+    /**
      * @return int[]
      */
     public function process(string $filePath, int $commentedLinesCountLimit): array
     {
         $commentedLines = [];
 
-        $fileLines = explode(PHP_EOL, FileSystem::read($filePath));
+        $fileLines = Strings::split(FileSystem::read($filePath), self::NEWLINE_REGEX);
 
         $commentLinesCount = 0;
 
