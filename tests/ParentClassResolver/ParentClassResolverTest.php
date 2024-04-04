11d1287d8d611bc394f64c1a538eaa8707ab0a77
Dorian Villet <gnutix@gmail.com>
2024-04-04 18:17:18 +0200

    ParentClassResolver: add tests for existing features. (#22)

---
+++ tests/ParentClassResolver/ParentClassResolverTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\SwissKnife\Tests\ParentClassResolver;
+
+use Rector\SwissKnife\Finder\PhpFilesFinder;
+use Rector\SwissKnife\ParentClassResolver;
+use Rector\SwissKnife\Tests\AbstractTestCase;
+
+final class ParentClassResolverTest extends AbstractTestCase
+{
+    private ParentClassResolver $parentClassResolver;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->parentClassResolver = $this->make(ParentClassResolver::class);
+    }
+
+    public function test(): void
+    {
+        $parentClasses = $this->parentClassResolver->resolve(PhpFilesFinder::find([__DIR__ . '/Fixture']), static function (): void {
+        });
+
+        $this->assertSame(
+            [
+                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\AbstractParentClass',
+                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClass',
+                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClassInSeparateNamespace',
+            ],
+            $parentClasses
+        );
+    }
+}
