dbe0e3115f0b17212ee589c72fe1625b29fe9687
Markus Staab <markus.staab@redaxo.de>
2024-04-04 18:40:17 +0200

    unify test expectations

--- tests/ParentClassResolver/ParentClassResolverTest.php
+++ tests/ParentClassResolver/ParentClassResolverTest.php
@@ -7,6 +7,9 @@ namespace Rector\SwissKnife\Tests\ParentClassResolver;
 use Rector\SwissKnife\Finder\PhpFilesFinder;
 use Rector\SwissKnife\ParentClassResolver;
 use Rector\SwissKnife\Tests\AbstractTestCase;
+use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\AbstractParentClass;
+use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClass;
+use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClassInSeparateNamespace;
 use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\SomeParentClass;
 
 final class ParentClassResolverTest extends AbstractTestCase
@@ -28,9 +31,9 @@ final class ParentClassResolverTest extends AbstractTestCase
         $this->assertSame(
             [
                 'ArrayObject',
-                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\AbstractParentClass',
-                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClass',
-                'Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClassInSeparateNamespace',
+                AbstractParentClass::class,
+                ParentClass::class,
+                ParentClassInSeparateNamespace::class,
                 SomeParentClass::class,
                 \SomeUnknownRootNamespaceClass::class, // @phpstan-ignore-line
             ],
