da6740d98e213df7ff6d0b75766efbf0f97f751a
Dorian Villet <gnutix@gmail.com>
2024-04-04 10:31:19 +0200

    ParentClassResolver: add tests for existing features.

---
+++ tests/ParentClassResolver/Fixture/SeparateNamespace/ChildOfParentClassInSeparateNamespace.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\SwissKnife\Tests\ParentClassResolver\Fixture\SeparateNamespace;
+
+use Rector\SwissKnife\Tests\ParentClassResolver\Fixture\ParentClassInSeparateNamespace;
+
+final class ChildOfParentClassInSeparateNamespace extends ParentClassInSeparateNamespace
+{
+}
