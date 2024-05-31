189f44b18a857f3fb20ecbeedc431681cb57eed4
GitHub Action <action@github.com>
2024-05-31 05:33:50 +0000

    release PHP 7.2 downgraded

--- src/Finder/FilesFinder.php
+++ src/Finder/FilesFinder.php
@@ -1,60 +1,44 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Rector\SwissKnife\Finder;
 
-use Symfony\Component\Finder\Finder;
-use Symfony\Component\Finder\SplFileInfo;
-
+use SwissKnife202405\Symfony\Component\Finder\Finder;
+use SwissKnife202405\Symfony\Component\Finder\SplFileInfo;
 final class FilesFinder
 {
     /**
      * @param string[] $sources
      * @return SplFileInfo[]
      */
-    public static function find(array $sources): array
+    public static function find(array $sources) : array
     {
         $paths = [];
         foreach ($sources as $source) {
-            $paths[] = getcwd() . DIRECTORY_SEPARATOR . $source;
+            $paths[] = \getcwd() . \DIRECTORY_SEPARATOR . $source;
         }
-
-        $finder = Finder::create()
-            ->files()
-            ->in($paths)
-            ->sortByName();
-
-        return iterator_to_array($finder->getIterator());
+        $finder = Finder::create()->files()->in($paths)->sortByName();
+        return \iterator_to_array($finder->getIterator());
     }
-
     /**
      * @param string[] $sources
      * @return SplFileInfo[]
      */
-    public static function findJsonFiles(array $sources): array
+    public static function findJsonFiles(array $sources) : array
     {
         $jsonFileInfos = [];
         $directories = [];
-
         foreach ($sources as $source) {
-            if (is_file($source)) {
+            if (\is_file($source)) {
                 $jsonFileInfos[] = new SplFileInfo($source, '', $source);
             } else {
                 $directories[] = $source;
             }
         }
-
-        $jsonFileFinder = Finder::create()
-            ->files()
-            ->in($directories)
-            ->name('*.json')
-            ->sortByName();
-
+        $jsonFileFinder = Finder::create()->files()->in($directories)->name('*.json')->sortByName();
         foreach ($jsonFileFinder->getIterator() as $fileInfo) {
             $jsonFileInfos[] = $fileInfo;
         }
-
         return $jsonFileInfos;
     }
 }
---
+++ vendor/illuminate/container/EntryNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SwissKnife202405\Illuminate\Container;
+
+use Exception;
+use SwissKnife202405\Psr\Container\NotFoundExceptionInterface;
+class EntryNotFoundException extends Exception implements NotFoundExceptionInterface
+{
+    //
+}
