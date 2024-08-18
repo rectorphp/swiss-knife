3f36b2df95e947dc686c6fa4c685a715faaeb00e
Tomas Votruba <tomas.vot@gmail.com>
2024-08-18 15:13:04 +0200

    Add ODM annotatoin/attribute based check (#41)
    * add document annotation based test
    
    * add ODM annotation/attribute detection

--- tests/EntityClassResolver/EntityClassResolverTest.php
+++ tests/EntityClassResolver/EntityClassResolverTest.php
@@ -6,6 +6,8 @@ namespace Rector\SwissKnife\Tests\EntityClassResolver;
 
 use Rector\SwissKnife\EntityClassResolver;
 use Rector\SwissKnife\Tests\AbstractTestCase;
+use Rector\SwissKnife\Tests\EntityClassResolver\Fixture\Anything\SomeAttributeDocument;
+use Rector\SwissKnife\Tests\EntityClassResolver\Fixture\Anything\SomeDocument;
 use Rector\SwissKnife\Tests\EntityClassResolver\Fixture\Entity\SomeEntity;
 
 final class EntityClassResolverTest extends AbstractTestCase
@@ -21,12 +23,26 @@ final class EntityClassResolverTest extends AbstractTestCase
 
     public function test(): void
     {
-        $entityClasses = $this->entityClassResolver->resolve([__DIR__ . '/Fixture'], static function (): void {
-        });
+        $entityClasses = $this->entityClassResolver->resolve(
+            [__DIR__ . '/Fixture/Entity', __DIR__ . '/Fixture/config'],
+            static function (): void {
+            }
+        );
 
         $this->assertSame(
             ['App\Some\Entity\Conference', 'App\Some\Entity\Project', 'App\Some\Entity\Talk', SomeEntity::class],
             $entityClasses
         );
+    }
+
+    public function testODMDocument(): void
+    {
+        $documentClasses = $this->entityClassResolver->resolve(
+            [__DIR__ . '/Fixture/Anything'],
+            static function (): void {
+            }
+        );
+
+        $this->assertSame([SomeAttributeDocument::class, SomeDocument::class], $documentClasses);
     }
 }
--- tests/EntityClassResolver/Fixture/Entity/SomeEntity.php
+++ tests/EntityClassResolver/Fixture/Entity/SomeEntity.php
@@ -2,6 +2,11 @@
 
 namespace Rector\SwissKnife\Tests\EntityClassResolver\Fixture\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
 class SomeEntity
 {
 }
