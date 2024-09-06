4dcbc9d89922ae00d0f906a64062f4c19fa9c21b
GitHub Action <action@github.com>
2024-09-06 10:08:42 +0000

    release PHP 7.2 downgraded

---
+++ vendor/illuminate/container/EntryNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SwissKnife202409\Illuminate\Container;
+
+use Exception;
+use SwissKnife202409\Psr\Container\NotFoundExceptionInterface;
+class EntryNotFoundException extends Exception implements NotFoundExceptionInterface
+{
+    //
+}
