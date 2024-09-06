0773b41f67c78f2ca1301a866393b8fcc428ba25
GitHub Action <action@github.com>
2024-09-06 09:51:52 +0000

    release PHP 7.2 downgraded

---
+++ vendor/illuminate/contracts/Encryption/EncryptException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SwissKnife202409\Illuminate\Contracts\Encryption;
+
+use RuntimeException;
+class EncryptException extends RuntimeException
+{
+    //
+}
---
+++ vendor/illuminate/contracts/Queue/ShouldBeEncrypted.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SwissKnife202409\Illuminate\Contracts\Queue;
+
+interface ShouldBeEncrypted
+{
+    //
+}
