71b19185baf36457d090b3ad081eb88b9e8d90e6
Tomas Votruba <tomas.vot@gmail.com>
2024-09-06 15:51:06 +0200

    Add twig extractor constant

---
+++ src/Twig/TwigTemplateConstantExtractor.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\SwissKnife\Twig;
+
+use Nette\Utils\FileSystem;
+use Nette\Utils\Strings;
+use Rector\SwissKnife\Contract\ClassConstantFetchInterface;
+use Rector\SwissKnife\ValueObject\ClassConstantFetch\ExternalClassAccessConstantFetch;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Webmozart\Assert\Assert;
+
+/**
+ * @see \Rector\SwissKnife\Tests\Twig\TwigTemplateConstantExtractor\TwigTemplateConstantExtractorTest
+ */
+final class TwigTemplateConstantExtractor
+{
+    /**
+     * @param string[] $directories
+     * @return ClassConstantFetchInterface[]
+     */
+    public function extractFromDirs(array $directories): array
+    {
+        $twigFileInfos = $this->findTwigFileInfosInDirectories($directories);
+
+        $classConstantFetches = [];
+        foreach ($twigFileInfos as $twigFileInfo) {
+            $currentClassConstantFetches = $this->findClassConstantFetchesInFile($twigFileInfo->getRealPath());
+
+            $classConstantFetches = array_merge($classConstantFetches, $currentClassConstantFetches);
+        }
+
+        return $classConstantFetches;
+    }
+
+    /**
+     * @return ClassConstantFetchInterface[]
+     */
+    private function findClassConstantFetchesInFile(string $filePath): array
+    {
+        $fileContents = FileSystem::read($filePath);
+
+        $constantMatches = Strings::matchAll($fileContents, '#{{\s*constant\(\s*([\'"])(?<constant>.*?)\1#');
+
+        $externalClassAccessConstantFetches = [];
+        foreach ($constantMatches as $constantMatch) {
+            [$className, $constantName] = explode('::', $constantMatch['constant']);
+            $className = str_replace('\\\\', '\\', $className);
+
+            $externalClassAccessConstantFetches[] = new ExternalClassAccessConstantFetch($className, $constantName);
+        }
+
+        return $externalClassAccessConstantFetches;
+    }
+
+    /**
+     * @param string[] $directories
+     * @return SplFileInfo[]
+     */
+    private function findTwigFileInfosInDirectories(array $directories): array
+    {
+        Assert::allString($directories);
+        Assert::allDirectory($directories);
+
+        $twigFinder = Finder::create()->files()->name('*.twig')
+            ->in($directories);
+
+        return iterator_to_array($twigFinder->getIterator());
+    }
+}
---
+++ tests/Twig/TwigTemplateConstantExtractor/Fixture/SomeTemplateUsedConstant.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Rector\SwissKnife\Tests\Twig\TwigTemplateConstantExtractor\Fixture;
+
+final class SomeTemplateUsedConstant
+{
+    public const STAY_PUBLIC = true;
+}
