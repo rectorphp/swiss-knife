1339c65233dad2d87b842c9efe654c15cfd2c69d
GitHub Action <action@github.com>
2024-09-06 15:10:41 +0000

    release PHP 7.2 downgraded

---
+++ vendor/illuminate/contracts/Mail/Factory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SwissKnife202409\Illuminate\Contracts\Mail;
+
+interface Factory
+{
+    /**
+     * Get a mailer instance by name.
+     *
+     * @param  string|null  $name
+     * @return \Illuminate\Contracts\Mail\Mailer
+     */
+    public function mailer($name = null);
+}
