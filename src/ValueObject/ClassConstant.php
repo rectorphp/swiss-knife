94618b24e706b0e6ec03673bb26f556adb54b7a3
GitHub Action <action@github.com>
2024-09-13 15:49:04 +0000

    release PHP 7.2 downgraded

--- src/ValueObject/ClassConstant.php
+++ src/ValueObject/ClassConstant.php
@@ -1,30 +1,36 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Rector\SwissKnife\ValueObject;
 
-use Webmozart\Assert\Assert;
-
-final readonly class ClassConstant
+use SwissKnife202409\Webmozart\Assert\Assert;
+final class ClassConstant
 {
+    /**
+     * @var class-string
+     * @readonly
+     */
+    private $className;
+    /**
+     * @readonly
+     * @var string
+     */
+    private $constantName;
     /**
      * @param class-string $className
      */
-    public function __construct(
-        private string $className,
-        private string $constantName,
-    ) {
+    public function __construct(string $className, string $constantName)
+    {
+        $this->className = $className;
+        $this->constantName = $constantName;
         Assert::notEmpty($constantName);
         Assert::notEmpty($className);
     }
-
-    public function getClassName(): string
+    public function getClassName() : string
     {
         return $this->className;
     }
-
-    public function getConstantName(): string
+    public function getConstantName() : string
     {
         return $this->constantName;
     }
---
+++ vendor/nikic/php-parser/lib/PhpParser/Comment/Doc.php
@@ -0,0 +1,8 @@
+<?php
+
+declare (strict_types=1);
+namespace SwissKnife202409\PhpParser\Comment;
+
+class Doc extends \SwissKnife202409\PhpParser\Comment
+{
+}
