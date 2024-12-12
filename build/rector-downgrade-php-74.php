bb3b899941d69ef24cd0117f2128d8036bef1270
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-12 18:32:42 +0700

    Update script for downgrade php 7.4 (#57)
    * Update script for downgrade php 7.4
    
    * Update script for downgrade php 7.4

--- build/rector-downgrade-php-74.php
+++ build/rector-downgrade-php-74.php
@@ -6,7 +6,7 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
+    $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_74]);
 
     $rectorConfig->skip([
         '*/Tests/*',
