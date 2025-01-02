a856e7662734a1b57f4fdbe3ef393d7cf74d297b
GitHub Action <action@github.com>
2025-01-02 14:48:26 +0000

    release PHP 7.2 downgraded

--- src/Testing/UnitTestFilter.php
+++ src/Testing/UnitTestFilter.php
@@ -1,7 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Rector\SwissKnife\Testing;
 
 final class UnitTestFilter
@@ -9,37 +8,28 @@ final class UnitTestFilter
     /**
      * @var string[]|class-string<KernelTestCase>[]
      */
-    private const NON_UNIT_TEST_CASE_CLASSES = [
-        'Symfony\Bundle\FrameworkBundle\Test\KernelTestCase',
-        'Symfony\Component\Form\Test\TypeTestCase',
-    ];
-
+    private const NON_UNIT_TEST_CASE_CLASSES = ['SwissKnife202501\\Symfony\\Bundle\\FrameworkBundle\\Test\\KernelTestCase', 'SwissKnife202501\\Symfony\\Component\\Form\\Test\\TypeTestCase'];
     /**
      * @param array<string, string> $testClassesToFilePaths
      * @return array<string, string>
      */
-    public function filter(array $testClassesToFilePaths): array
+    public function filter(array $testClassesToFilePaths) : array
     {
-        return array_filter(
-            $testClassesToFilePaths,
-            fn (string $testClass): bool => $this->isUnitTest($testClass),
-            ARRAY_FILTER_USE_KEY
-        );
+        return \array_filter($testClassesToFilePaths, function (string $testClass) : bool {
+            return $this->isUnitTest($testClass);
+        }, \ARRAY_FILTER_USE_KEY);
     }
-
-    private function isUnitTest(string $class): bool
+    private function isUnitTest(string $class) : bool
     {
-        if (! is_a($class, 'PHPUnit\Framework\TestCase', true) && ! is_a($class, 'PHPUnit_Framework_TestCase', true)) {
-            return false;
+        if (!\is_a($class, 'SwissKnife202501\\PHPUnit\\Framework\\TestCase', \true) && !\is_a($class, 'SwissKnife202501\\PHPUnit_Framework_TestCase', \true)) {
+            return \false;
         }
-
         foreach (self::NON_UNIT_TEST_CASE_CLASSES as $nonUnitTestCaseClass) {
             // required special behavior
-            if (is_a($class, $nonUnitTestCaseClass, true)) {
-                return false;
+            if (\is_a($class, $nonUnitTestCaseClass, \true)) {
+                return \false;
             }
         }
-
-        return true;
+        return \true;
     }
 }
