a2d6810d84d98db794d517773bae9f0270388aac
Tomas Votruba <tomas.vot@gmail.com>
2025-01-17 10:23:42 +0000

    use cache + colors (#73)
    * use cache
    
    * add missing lable, to show
    
    * colors
    
    * shorten longer names, to make all packages fit
    
    * tidy up to collection
    
    * decouple symfony column styler

--- src/Command/MultiPackageComposerStatsCommand.php
+++ src/Command/MultiPackageComposerStatsCommand.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Rector\SwissKnife\Command;
 
+use Nette\Utils\Strings;
 use Rector\SwissKnife\Composer\ComposerJsonResolver;
+use Rector\SwissKnife\Helper\SymfonyColumnStyler;
 use Rector\SwissKnife\Sorting\ArrayFilter;
-use Rector\SwissKnife\ValueObject\ComposerJson;
+use Rector\SwissKnife\ValueObject\ComposerJsonCollection;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\TableStyle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -16,6 +19,11 @@ use Webmozart\Assert\Assert;
 
 final class MultiPackageComposerStatsCommand extends Command
 {
+    /**
+     * @var string
+     */
+    private const MISSING_LABEL = '*MISSING*';
+
     public function __construct(
         private readonly SymfonyStyle $symfonyStyle,
         private readonly ComposerJsonResolver $composerJsonResolver,
@@ -43,61 +51,59 @@ final class MultiPackageComposerStatsCommand extends Command
         $repositories = (array) $input->getArgument('repositories');
         Assert::allString($repositories);
 
-        $this->symfonyStyle->note(sprintf(
-            'Downloading "composer.json" files for %d repositories',
+        $this->symfonyStyle->title(sprintf(
+            'Loading "composer.json" files for %d repositories',
             count($repositories)
         ));
 
-        $projectsComposerJsons = $this->composerJsonResolver->resolveFromRepositories($repositories);
-        $tableHeadlines = $this->resolveTableHeadlines($projectsComposerJsons);
+        $composerJsonCollection = $this->composerJsonResolver->resolveFromRepositories($repositories);
 
-        $requiredPackageNames = $this->resolveProjectsRequiredPackageNames($projectsComposerJsons);
+        $tableHeadlines = array_merge(['dependency'], $composerJsonCollection->getRepositoryNames());
+        $requiredPackageNames = $composerJsonCollection->getRequiredPackageNames();
+
+        $tableRows = $this->createTableRows($requiredPackageNames, $composerJsonCollection);
+
+        $table = $this->symfonyStyle->createTable()
+            ->setHeaders($tableHeadlines)
+            ->setRows($tableRows);
+
+        for ($i = 1; $i < count($tableHeadlines); $i++) {
+            $table->setColumnStyle($i, (new TableStyle())->setPadType(STR_PAD_LEFT));
+        }
+
+        $table->render();
+        $this->symfonyStyle->newLine();
+
+        return self::SUCCESS;
+    }
+
+    /**
+     * @param string[] $requiredPackageNames
+     * @return array<mixed[]>
+     */
+    private function createTableRows(array $requiredPackageNames, ComposerJsonCollection $composerJsonCollection): array
+    {
         $tableRows = [];
 
         foreach ($requiredPackageNames as $requiredPackageName) {
-            $tableRow = [$requiredPackageName];
+            $shortRequiredPackageName = Strings::truncate($requiredPackageName, 22);
+            $tableRow = [$shortRequiredPackageName];
 
-            foreach ($projectsComposerJsons as $composerJson) {
-                $tableRow[] = $composerJson->getPackageVersion($requiredPackageName);
+            foreach ($composerJsonCollection->all() as $composerJson) {
+                $packageVersion = $composerJson->getPackageVersion($requiredPackageName);
+
+                // special case for PHP
+                if ($requiredPackageName === 'php' && $packageVersion === null) {
+                    $tableRow[] = SymfonyColumnStyler::createRedCell(self::MISSING_LABEL);
+                } else {
+                    $tableRow[] = $packageVersion;
+                }
             }
 
             $tableRows[] = $tableRow;
         }
 
         // sort and put those with both values first
-        $tableRows = ArrayFilter::filterOnlyAtLeast2($tableRows);
-
-        $this->symfonyStyle->table($tableHeadlines, $tableRows);
-
-        return self::SUCCESS;
-    }
-
-    /**
-     * @param ComposerJson[] $projectsComposerJsons
-     * @return string[]
-     */
-    private function resolveTableHeadlines(array $projectsComposerJsons): array
-    {
-        $tableHeadlines = ['required dependency'];
-
-        foreach ($projectsComposerJsons as $composerJson) {
-            $tableHeadlines[] = $composerJson->getRepositoryName();
-        }
-
-        return $tableHeadlines;
-    }
-
-    /**
-     * @param ComposerJson[] $projectsComposerJsons
-     * @return string[]
-     */
-    private function resolveProjectsRequiredPackageNames(array $projectsComposerJsons): array
-    {
-        $requiredPackageNames = [];
-        foreach ($projectsComposerJsons as $composerJson) {
-            $requiredPackageNames = array_merge($requiredPackageNames, $composerJson->getRequiredPackageNames());
-        }
-
-        return array_unique($requiredPackageNames);
+        return ArrayFilter::filterOnlyAtLeast2($tableRows);
     }
 }
--- src/Composer/ComposerJsonResolver.php
+++ src/Composer/ComposerJsonResolver.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace Rector\SwissKnife\Composer;
 
 use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
 use Rector\SwissKnife\ValueObject\ComposerJson;
+use Rector\SwissKnife\ValueObject\ComposerJsonCollection;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Webmozart\Assert\Assert;
 
 final class ComposerJsonResolver
@@ -15,34 +18,67 @@ final class ComposerJsonResolver
      */
     private const TEMP_PROJECT_COMPOSER_JSON = 'temp-project-composer.json';
 
+    public function __construct(
+        private readonly SymfonyStyle $symfonyStyle
+    ) {
+    }
+
     /**
      * @param string[] $repositories
-     * @return ComposerJson[]
      */
-    public function resolveFromRepositories(array $repositories): array
+    public function resolveFromRepositories(array $repositories): ComposerJsonCollection
     {
         Assert::allString($repositories);
 
         $projectsComposerJsons = [];
         foreach ($repositories as $repository) {
-            // clones only "composer.json" file
-            exec(sprintf(
-                'git archive --remote=%s HEAD composer.json | tar -xO composer.json > %s',
-                $repository,
-                self::TEMP_PROJECT_COMPOSER_JSON
-            ));
-
-            $projectsComposerJsonContents = FileSystem::read(self::TEMP_PROJECT_COMPOSER_JSON);
-
-            Assert::string($projectsComposerJsonContents);
-            Assert::notEmpty($projectsComposerJsonContents);
-
-            $projectsComposerJsons[] = new ComposerJson($repository, $projectsComposerJsonContents);
+            $projectsComposerJson = $this->getComposerJsonForRepository($repository, true);
+            $projectsComposerJsons[] = new ComposerJson($repository, $projectsComposerJson);
         }
 
         // tidy up temporary file
         FileSystem::delete(self::TEMP_PROJECT_COMPOSER_JSON);
 
-        return $projectsComposerJsons;
+        return new ComposerJsonCollection($projectsComposerJsons);
+    }
+
+    /**
+     * @return array{"require": mixed[], "require-dev": mixed[]}
+     */
+    private function getComposerJsonForRepository(string $repository, bool $useCache): array
+    {
+        // allow cache to avoid long staling
+        $cacheFilePath = sys_get_temp_dir() . '/multi-composer-json/' . md5($repository) . '.json';
+        if ($useCache === true && file_exists($cacheFilePath)) {
+            $fileContents = FileSystem::read($cacheFilePath);
+            return Json::decode($fileContents, Json::FORCE_ARRAY);
+        }
+
+        $this->symfonyStyle->writeln('Loading for ' . $repository);
+
+        // clone only "composer.json" file
+        exec(sprintf(
+            'git archive --remote=%s HEAD composer.json | tar -xO composer.json > %s',
+            $repository,
+            self::TEMP_PROJECT_COMPOSER_JSON
+        ));
+
+        $projectsComposerJsonContents = FileSystem::read(self::TEMP_PROJECT_COMPOSER_JSON);
+
+        // unset all but require and require-dev sections
+        Assert::string($projectsComposerJsonContents);
+        Assert::notEmpty($projectsComposerJsonContents);
+
+        $projectsComposerJson = Json::decode($projectsComposerJsonContents, Json::FORCE_ARRAY);
+
+        // store only necessary data
+        $bareComposerJson = [
+            'require' => $projectsComposerJson['require'] ?? [],
+            'require-dev' => $projectsComposerJson['require-dev'] ?? [],
+        ];
+
+        FileSystem::write($cacheFilePath, Json::encode($bareComposerJson, pretty: true));
+
+        return $bareComposerJson;
     }
 }
