7e8b56a10acdae9774fd6bb09f2a47f578c6cd95
Markus Staab <markus.staab@redaxo.de>
2025-05-20 12:28:23 +0200

    Update PhpFilesFinderTest.php

--- tests/Finder/PhpFilesFinderTest.php
+++ tests/Finder/PhpFilesFinderTest.php
@@ -10,27 +10,29 @@ final class PhpFilesFinderTest extends AbstractTestCase
 {
     public function testExcludeByFnMatch(): void
     {
-        $finder = \Rector\SwissKnife\Finder\PhpFilesFinder::find([__DIR__ . '/Fixture'], ['*Controller.php']);
+        $files = \Rector\SwissKnife\Finder\PhpFilesFinder::find([__DIR__ . '/Fixture'], ['*Controller.php']);
 
-        $this->assertSame(1, count($finder));
+        $this->assertSame(1, count($files));
 
-        $file = array_pop($finder);
+        sort($files); // make order predictable across operating systems
+        $file = array_pop($files);
         $this->assertNotNull($file);
         $this->assertStringContainsString('AModel.php', $file->getRealPath());
     }
 
     public function testExcludeByFnMatch2(): void
     {
-        $finder = \Rector\SwissKnife\Finder\PhpFilesFinder::find([__DIR__ . '/Fixture'], ['*Model.php']);
+        $files = \Rector\SwissKnife\Finder\PhpFilesFinder::find([__DIR__ . '/Fixture'], ['*Model.php']);
 
-        $this->assertSame(2, count($finder));
+        $this->assertSame(2, count($files));
 
-        $file = array_pop($finder);
-        $this->assertNotNull($file);
-        $this->assertStringContainsString('AController.php', $file->getRealPath());
-
-        $file = array_pop($finder);
+        sort($files); // make order predictable across operating systems
+        $file = array_pop($files);
         $this->assertNotNull($file);
         $this->assertStringContainsString('BController.php', $file->getRealPath());
+
+        $file = array_pop($files);
+        $this->assertNotNull($file);
+        $this->assertStringContainsString('AController.php', $file->getRealPath());
     }
 }
